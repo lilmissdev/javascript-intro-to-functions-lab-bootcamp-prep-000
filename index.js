f994701db8755e675018080ca3896d0bcd65d204
Lisa Connell <connell.lisamarie@gmail.com>
2018-03-09 21:02:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,4 +19,6 @@ function sayHiToGrandma(string) {
   return "I love you, too."
   else if (string ===string.toLowerCase())
   return "I can't hear you!"
+  else if (string ===string.toUpperCase())
+  return "YES INDEED!"
 }
